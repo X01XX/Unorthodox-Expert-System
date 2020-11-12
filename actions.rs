afa31f2b57cbf1c2e03a6fe91d81578b9f73684b
X01XX <47160211+X01XX@users.noreply.github.com>
2020-11-12 15:12:59 -0600

    Add files via upload
    Changes to improve multiple result testing.

--- actions.rs
+++ actions.rs
@@ -8,38 +8,32 @@ use crate::state::SomeState;
 // The number of bits is set by the line of code that creates
 // the first current state, with 1, or more, u8 integers.
 
-pub fn action0(cur: &SomeState, _hv: usize) -> SomeState {
+pub fn action0(cur: &SomeState, hv: usize) -> SomeState {
     let num = 0;
-
-    //println!("action 0: hv {}", hv);
-    //    if hv == 1 {
-    //        let new_state = cur.toggle_bits(&[0]);
-    //
-    //        println!("\nAct {}  {} -> {}", num, cur, &new_state);
-    //        return new_state;
-    //    }
-    //    let statex = cur.toggle_bits(&[2]);
-    //    println!("\nAct {}  {} -> {}", num, cur, &statex);
-    //    statex
-    //cur.clone()
 
     if cur.is_bit_set(3) && cur.is_bit_set(1) == false     // 1X0X
         || cur.is_bit_set(3) == false && cur.is_bit_set(1) // 0X1X
-        || cur.is_bit_set(2) && cur.is_bit_set(0)
-    // X1X1
-    {
-        //    if num_seen % 2 == 1 {
-        let new_state = cur.toggle_bits(&[0]);
-        println!("\nAct {}  {} -> {}", num, cur, new_state);
-        return new_state;
-        //        }
+        || cur.is_bit_set(2) && cur.is_bit_set(0)  {       // X1X1
+		
+		// When the action is created, changing the hv parm (0,1,2), in the
+		// main file will have the effect of changing these groups to have
+		// 1, 2 or 3 results.
+		if hv == 0 {
+		    let new_state = cur.toggle_bits(&[0]);
+            println!("\nAct {}  {} -> {}", num, cur, new_state);
+            return new_state;
+	    } else if hv == 1 {
+		    let new_state = cur.toggle_bits(&[1]);
+            println!("\nAct {}  {} -> {}", num, cur, new_state);
+            return new_state;			
+		} else if hv == 2 {
+		    let new_state = cur.toggle_bits(&[2]);
+            println!("\nAct {}  {} -> {}", num, cur, new_state);
+            return new_state;			
+		}       
+        
     }
 
-    //else if num_seen % 3 == 2 {
-    //    let new_state = cur.toggle_bits(&[2]);
-    //    println!("\nAct {}  {} -> {}", num, cur, new_state);
-    //    return new_state;
-    //	}
     println!("\nAct {}  {} -> {}", num, cur, cur);
     cur.clone()
 }
